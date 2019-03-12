e90bd0b95401b7abe721db5c7fcb35f2cead6bad
Tyler Lentz <lentz.etyler@gmail.com>
2019-03-12 02:56:14 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -10,27 +10,3 @@ function teenager(age) {
   return "You are not a teenager";
 }
 }
-
-function ageChecker(age) {
-if (age <= 19 && age =>13){
-  console.log("You are a teenager");
-} else if (age < 13){
- console.log("You are a kid");
-} else{
-  return "You are a grownup";
-}
-}
-
-function ternaryTeenager(age) {
-age => 13 && age <= 19 ? return "You are a teenager" : "You are not a teenager"
-}
-
-function switchAge(age) {
-switch (age)
-case age <= 19 && age => 13:
-console.log("You are a teenager");
-break;
-default:
-return "You have an age";
-break;
-}
